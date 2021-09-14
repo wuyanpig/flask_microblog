ba6083e65c5a00b39f800acc60a666d2ba960719
Yan Wu <wuyanpig@hotmail.com>
2021-09-14 17:18:40 +0800

    test py commit

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,18 @@
 from app import app
+from flask import render_template#从flask包中导入render_template函数
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'username':'Miguel'}
-	return '''
-<html>
-	<head>
-		<title>Home Page - Microblog</title>
-	<head>
-	<body>
-		<h1>Hello,''' + user['username']+ '''!</h1>
-	<body>
-</html>
-'''
+	posts = [
+		{
+			'author': {'username':'John'},
+			'body':'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username':'Susan'},
+			'body':'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
