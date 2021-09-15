cd501015223fb2e968316caa4e6fd7fb546ab056
Yan Wu <wuyanpig@hotmail.com>
2021-09-15 11:18:37 +0800

    db related commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from config import Config#从config模块导入Config类
 
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-#放在下面，不放在开头，是为了避免循环引用
-from app import routes
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
+from app import routes,models
+#导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
