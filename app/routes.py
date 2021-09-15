69454c83375447d753103d09f793febd56269f65
Yan Wu <wuyanpig@hotmail.com>
2021-09-15 10:30:49 +0800

    form finished

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template#从flask包中导入render_template函数
+from flask import render_template, flash, redirect, url_for  # 从flask包中导入render_template函数
+
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +19,11 @@ def index():
 		}
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for 用户 {},记不记remember_me={}'.format(form.username.data,form.remember_me.data))
+		return redirect(url_for(index))
+	return render_template('login.html',title='Sign In',form=form)
