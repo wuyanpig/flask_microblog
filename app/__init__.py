2810be66b83bdec7e48890aa03a123044ae2c4d4
Yan Wu <wuyanpig@hotmail.com>
2021-09-15 10:31:02 +0800

    form finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
+from config import Config#从config模块导入Config类
 
-app= Flask(__name__)
-
-print('ttttttttttttttttttt:',__name__)
-
+app = Flask(__name__)
+app.config.from_object(Config)
 
 #放在下面，不放在开头，是为了避免循环引用
 from app import routes
