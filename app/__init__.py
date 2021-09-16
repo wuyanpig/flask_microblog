77b945491bd889cbbb4ffe80c9870e3d5e50e441
Yan Wu <wuyanpig@hotmail.com>
2021-09-16 16:21:15 +0800

    login logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_login import LoginManager
+
 from config import Config#从config模块导入Config类
 
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
@@ -9,6 +11,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes,models
 #导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
