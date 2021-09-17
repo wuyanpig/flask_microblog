f66d1fd5b5a6fe2dbcb1bf3e1dc9508e6d2ce964
Yan Wu <wuyanpig@hotmail.com>
2021-09-17 15:52:55 +0800

    profile and error function

--- app/__init__.py
+++ app/__init__.py
@@ -14,5 +14,5 @@ migrate = Migrate(app, db)#迁移引擎对象
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes,models
+from app import routes,models,errors
 #导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
