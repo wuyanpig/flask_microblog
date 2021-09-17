805e9edb8250e90320ce7be3a348dedacfb28e83
Yan Wu <wuyanpig@hotmail.com>
2021-09-17 17:43:08 +0800

    profile and error function

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from flask import Flask
 from flask_login import LoginManager
 
@@ -5,6 +8,7 @@ from config import Config#从config模块导入Config类
 
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +17,22 @@ db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
 login = LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
 
 from app import routes,models,errors
 #导入一个新模块models，它将定义数据库的结构，目前为止尚未编写
